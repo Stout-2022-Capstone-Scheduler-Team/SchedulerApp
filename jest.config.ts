b11eef14f58c4f35eadde19692959fe85e5ebafa
Matthew Pomes <matthew.pomes@pm.me>
2022-10-12 10:21:14 -0500

    Configure Code Coverage

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import nextJest from 'next/jest'
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+export default createJestConfig({
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './components/**',
+    './entities/**',
+    './interfaces/**',
+    './pages/**',
+    './public/**',
+    './services/**',
+    './styles/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
+})
