4dbd697c4f82f5c3c238eb55dd9f342bbfd8bc85
Matthew Pomes <matthew.pomes@pm.me>
2022-10-12 14:48:47 -0500

    Complete initial testing round

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+/* istanbul ignore file */
+// This file is not testable without spinning up an entire server,
+// so it has been exluded from coverage results
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
