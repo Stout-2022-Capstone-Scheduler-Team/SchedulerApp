45ce70fec86cbd12f659ed523c9700716bc8e052
Matthew Pomes <matthew.pomes@pm.me>
2022-10-17 14:31:47 -0500

    Fix Eslint issues

--- __test__/services/collapse.test.ts
+++ __test__/services/collapse.test.ts
@@ -1,7 +1,5 @@
-import { Time, Shift, DayOftheWeek, Employee } from '../../services/types'
-
 import { WaveformCollapseAlgorithm } from '../../services/waveform_collapse'
-import { shift, all_day, Monday, Tuesday, person } from './utils'
+import { shift, allDay, Monday, Tuesday, person } from './utils'
 
 test('Empty Schedule', () => {
   const process = new WaveformCollapseAlgorithm([], [])
@@ -16,13 +14,13 @@ test('Collapse Schedule', () => {
       shift('14:00', '16:00', Tuesday),
     ],
     [
-      person('alice', 1, 4, [all_day(Monday), all_day(Tuesday)]),
-      person('bob', 1, 12, [all_day(Monday), all_day(Tuesday)]),
+      person('alice', 1, 4, [allDay(Monday), allDay(Tuesday)]),
+      person('bob', 1, 12, [allDay(Monday), allDay(Tuesday)]),
       person('clair', 1, 12, [shift('12:00', '24:00', Tuesday)]),
     ]
   )
   expect(process.generate()).toBe(true)
-  let s = process.getSchedule()
+  const s = process.getSchedule()
   expect(s[0].owner).toBe('alice')
   expect(s[1].owner).toBe('bob')
   expect(s[2].owner).toBe('clair')
@@ -37,14 +35,14 @@ test('Jarod 1 Schedule', () => {
       shift('09:00', '10:00', Monday),
     ],
     [
-      person('alice', 2, 12, [all_day(Monday)]),
-      person('bob', 1, 12, [all_day(Monday)]),
-      person('clair', 3, 12, [all_day(Monday)]),
+      person('alice', 2, 12, [allDay(Monday)]),
+      person('bob', 1, 12, [allDay(Monday)]),
+      person('clair', 3, 12, [allDay(Monday)]),
     ]
   )
   expect(process.generate()).toBe(true)
   // expect(process.getSchedule()).toBe([])
-  let s = process.getSchedule()
+  const s = process.getSchedule()
   expect(s[0].owner).toBe('alice')
   expect(s[1].owner).toBe('alice')
   expect(s[2].owner).toBe('bob')
@@ -59,10 +57,10 @@ test('Impossible Schedule', () => {
       shift('09:00', '10:00', Monday),
       shift('11:00', '12:00', Monday),
     ],
-    [person('alice', 2, 2, [all_day(Monday)])]
+    [person('alice', 2, 2, [allDay(Monday)])]
   )
   expect(process.generate()).toBe(false)
-  let s = process.getSortedSchedule()
+  const s = process.getSortedSchedule()
   expect(s[0].owner).toBe('alice')
   expect(s[1].owner).toBe('alice')
   expect(s[2].owner).toBe('')
