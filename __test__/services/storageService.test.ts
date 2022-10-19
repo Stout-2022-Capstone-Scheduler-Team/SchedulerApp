37038c192d1a13f1e73947282984db03a6fe598b
Matthew Pomes <matthew.pomes@pm.me>
2022-10-19 14:21:38 -0500

    Add mock test

--- __test__/services/storageService.test.ts
+++ __test__/services/storageService.test.ts
@@ -1,5 +1,11 @@
 import { StorageService } from "../../services/storageService";
+import foragemock from "../../__mocks__/localforage";
 
-test("return all", () => {
-  expect(true).toBe(false);
+test("get item", async () => {
+  let a = {};
+  foragemock.getItem.mockResolvedValueOnce(a);
+
+  let storage = new StorageService();
+  expect(await storage.read("item")).toBe(a);
+  expect(foragemock.getItem.mock.calls).toEqual([["item"]]);
 });
--- services/storageService.ts
+++ services/storageService.ts
@@ -8,62 +8,31 @@ include method for getting all local storage schedules
 
 CRUD functionality for local storage with schedules*/
 
+type Schedule = any;
+
 export class StorageService {
   //getting specific schedule based on key data
-  read(schedule: { name: string }) {
+  async read(name: string): Promise<Schedule> {
     // The same code, but using ES6 Promises.
-    localforage
-      .iterate(function (value, key, iterationNumber) {
-        // Resulting key/value pair -- this callback
-        // will be executed for every item in the
-        // database.
-        console.log([key, value]);
-      })
-      .then(function () {
-        console.log("Iteration has completed");
-      })
-      .catch(function (err) {
-        // This code runs if there were any errors
-        console.log(err);
-      });
+    return await localforage.getItem(name);
   }
 
   //reading the schedule list and seeing if the current schedule is already in the storage
   //if it isnt then add the currently held schedule to the schedule list
-  update(schedule: { name: string }) {
-    this.read(schedule);
-    if (schedule.name === schedule.name)
-      localforage.setItem(schedule.name, schedule);
-    else localforage.setItem(schedule.name, schedule);
+  update(name: string, val: Schedule) {
+    localforage.setItem(name, val);
   }
 
   //deleting a specific schedule list and seeing if the specified schedule is even in the storage
-  delete(schedule: { name: string }) {
-    this.read(schedule);
-    if (schedule.name === schedule.name) {
-      localforage
-        .removeItem(schedule.name)
-        .then(function () {
-          // Run this code once the key has been removed.
-          console.log("Key is cleared!");
-        })
-        .catch(function (err) {
-          // This code runs if there were any errors
-          console.log(err);
-        });
-    }
+  delete(name: string) {
+    localforage.removeItem(name);
   }
 
-  returnAll() {
-    localforage
-      .keys()
-      .then(function (keys) {
-        // An array of all the key names.
-        console.log(keys);
-      })
-      .catch(function (err) {
-        // This code runs if there were any errors
-        console.log(err);
-      });
+  async returnAll(): Promise<{ [key: string]: Schedule }> {
+    let ret: { [key: string]: Schedule } = {};
+    await localforage.iterate((value: Schedule, key: string, _: number) => {
+      ret[key] = value;
+    });
+    return ret;
   }
 }
