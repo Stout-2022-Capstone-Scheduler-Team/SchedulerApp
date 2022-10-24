0019f6ce479e1f9757f06f3eef257caca4c3b2f5
Louie Mattia <mattial8709@my.uwstout.edu>
2022-10-24 14:22:12 -0500

    Finished start page and added tests

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,10 +1,26 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 
-import Home from '../pages/index'
+import Home from "../pages/index";
 
-test('renders learn react link', () => {
-  render(<Home />)
-  const linkElement = screen.getByText(/This is the home/i)
-  expect(linkElement).toBeInTheDocument()
-})
+import renderer from "react-test-renderer";
+
+test("Home page snapshot", () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Create Schedule Button", () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Create Schedule/i);
+  expect(linkElement).toBeVisible();
+  expect(linkElement).toBeEnabled();
+  expect(linkElement).toHaveAttribute("href", "/create");
+});
+
+test("Advanced Options Button", () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Advanced Options/i);
+  expect(linkElement).toBeVisible();
+  expect(linkElement).toBeEnabled();
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
-import { Button, Box, Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 
 export default function Home(): JSX.Element {
   return (
-    <Box sx={{ mx: "7%" }}>
+    <Stack sx={{ mx: "7%" }} alignItems="center">
       <h1>Welcome to the Scheduler Builder</h1>
-      <Stack spacing={2}>
+      <Stack spacing={2} sx={{ width: 300 }}>
         <Stack direction="column" spacing={2}>
-          <Button variant="contained" color="primary" a href="google.com" >
+          <Button variant="contained" color="secondary" sx={{ typography: "h5", padding: 2 }} href = "/create">
             Create Schedule
           </Button>
         </Stack>
-        <Stack direction="row" spacing={2}>
-          <Button variant="contained" color="secondary">
+        <Stack direction="column" spacing={2} alignItems="center">
+          <Button variant="contained" color="primary">
             Advanced Options
           </Button>
         </Stack>
       </Stack>
-    </Box>
+    </Stack>
   );
 }
