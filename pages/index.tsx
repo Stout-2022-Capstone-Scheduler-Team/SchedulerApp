471f1428dce7aef16020bd8e0d67d6424ca089a0
Louie Mattia <mattial8709@my.uwstout.edu>
2022-10-24 12:49:28 -0500

    Edited index page to start page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,16 @@ import { Button, Box, Stack } from "@mui/material";
 export default function Home(): JSX.Element {
   return (
     <Box sx={{ mx: "7%" }}>
-      <h1>This is the home</h1>
+      <h1>Welcome to the Scheduler Builder</h1>
       <Stack spacing={2}>
-        <Stack direction="row" spacing={2}>
-          <Button variant="contained">Primary</Button>
-          <Button variant="contained" color="secondary">
-            Secondary
+        <Stack direction="column" spacing={2}>
+          <Button variant="contained" color="primary" a href="google.com" >
+            Create Schedule
           </Button>
         </Stack>
         <Stack direction="row" spacing={2}>
-          <Button variant="contained">Primary Dark</Button>
           <Button variant="contained" color="secondary">
-            Secondary Dark
+            Advanced Options
           </Button>
         </Stack>
       </Stack>
