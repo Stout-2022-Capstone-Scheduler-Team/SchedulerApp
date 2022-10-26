5f2ed545988894c1276db5a81e1b442f0f2b63ed
Louie Mattia <mattial8709@my.uwstout.edu>
2022-10-26 13:00:24 -0500

    made single stack for start, updated test snapshot

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,14 @@ import { Button, Stack } from "@mui/material";
 
 export default function Home(): JSX.Element {
   return (
-    <Stack sx={{ mx: "7%" }} alignItems="center">
+    <Stack sx={{ mx: "7%" }} alignItems="center" spacing={2}>
       <h1>Welcome to the Scheduler Builder</h1>
-      <Stack spacing={2} sx={{ width: 300 }}>
-        <Stack direction="column" spacing={2}>
-          <Button variant="contained" color="secondary" sx={{ typography: "h5", padding: 2 }} href = "/create">
+          <Button variant="contained" color="secondary" sx={{ typography: "h5", padding: 2, width: 300 }} href = "/create">
             Create Schedule
           </Button>
-        </Stack>
-        <Stack direction="column" spacing={2} alignItems="center">
           <Button variant="contained" color="primary">
             Advanced Options
           </Button>
-        </Stack>
-      </Stack>
     </Stack>
   );
 }
