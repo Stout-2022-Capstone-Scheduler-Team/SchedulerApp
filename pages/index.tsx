ad0a823e5766e73ecbadc4e65a3980d953de4b9f
YoungJoel <joeleyoung01@gmail.com>
2022-10-26 14:34:03 -0500

    fix index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import { Button, Box, Stack } from "@mui/material";
-
-import EditSchedule from "./edit-schedule";
+import { Button, Stack } from "@mui/material";
 
 export default function Home(): JSX.Element {
   return (
-    <>
-      <EditSchedule />
-      {/* ADD logic to switch between pages */}
-    </>
+    <Stack sx={{ mx: "7%" }} alignItems="center" spacing={2}>
+      <h1>Welcome to the Scheduler Builder</h1>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ typography: "h5", padding: 2, width: 300 }}
+        href="/edit-schedule"
+      >
+        Create Schedule
+      </Button>
+      <Button variant="contained" color="primary">
+        Advanced Options
+      </Button>
+    </Stack>
   );
 }
