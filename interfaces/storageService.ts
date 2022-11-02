fef8ea405c657a86096dd659129a3d3186eb44b7
Matthew Pomes <matthew.pomes@pm.me>
2022-11-02 14:29:43 -0500

    Fix imports

--- interfaces/storageService.ts
+++ interfaces/storageService.ts
@@ -6,18 +6,18 @@ include method for getting all local storage schedules
 
 CRUD functionality for local storage with schedules */
 
-import { Schedule } from "../services/storageService";
+import Scheduler from "../interfaces/scheduler";
 
 export default interface StorageService {
   // getting specific schedule based on key data
-  read: (name: string) => Promise<Schedule>;
+  read: (name: string) => Promise<Scheduler | null>;
   // reading the schedule list and seeing if the current schedule is already in the storage
   // if it isnt then add the currently held schedule to the schedule list
-  update: (name: string, val: Schedule) => Promise<void>;
+  update: (name: string, val: Scheduler) => Promise<void>;
 
   // deleting a specific schedule list and seeing if the specified schedule is even in the storage
   delete: (name: string) => Promise<void>;
 
   // getting all of the schedules from the storage and returning them
-  returnAll: () => Promise<{ [key: string]: Schedule }>;
+  returnAll: () => Promise<{ [key: string]: Scheduler }>;
 }
