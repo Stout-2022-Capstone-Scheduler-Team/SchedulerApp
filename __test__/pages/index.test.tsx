f97619244e42632d24d2197d58113825e6c96deb
daccola <78117344+daccola@users.noreply.github.com>
2022-11-02 13:21:32 -0500

    added snapshot test

---
+++ __test__/pages/index.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+import EditSchedule from "../../pages/edit-schedule";
+
+test("EditSchedule Renders", () => {
+  const editSchedule = renderer.create(<EditSchedule/>);
+  expect(editSchedule).toMatchSnapshot();
+});
