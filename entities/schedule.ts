ce5cec518b17f3d0e47320e7901fbe8acd00d1d5
YoungJoel <joeleyoung01@gmail.com>
2022-11-07 14:37:42 -0600

    updated the schedule object to include setters and getters as expected

--- entities/schedule.ts
+++ entities/schedule.ts
@@ -1,11 +1,11 @@
 import { Employee, Shift } from "./types";
 
 export class Schedule {
-  private staff: Employee[] = [];
-  private shifts: Shift[] = [];
-  private assignedShifts: Shift[] = [];
-  private minHoursWorked: number;
-  private maxHoursWorked: number;
+  private _staff: Employee[] = [];
+  private _shifts: Shift[] = [];
+  private _assignedShifts: Shift[] = [];
+  private _minHoursWorked: number;
+  private _maxHoursWorked: number;
 
   /**
    *
@@ -13,8 +13,24 @@ export class Schedule {
    * @param maxHours Maximum man hours for this week
    */
   constructor(minHours = 0, maxHours = 400) {
-    this.minHoursWorked = minHours;
-    this.maxHoursWorked = maxHours;
+    this._minHoursWorked = minHours;
+    this._maxHoursWorked = maxHours;
+  }
+
+  public get minHoursWorked() {
+    return this._minHoursWorked;
+  }
+
+  public set minHoursWorked(newValue) {
+    this._minHoursWorked = newValue;
+  }
+
+  public get maxHoursWorked() {
+    return this._maxHoursWorked;
+  }
+
+  public set maxHoursWorked(newValue) {
+    this._maxHoursWorked = newValue;
   }
 
   /**
@@ -22,15 +38,25 @@ export class Schedule {
    * @param newShift New shift to add
    */
   addShift(newShift: Shift) {
-    this.shifts.concat(newShift);
+    this._shifts.concat(newShift);
+  }
+
+  /**
+   * Remove a given shift from the schedule
+   * @param shiftToRemove Shift to remove
+   * @returns shift that was removed
+   */
+  removeShift(shiftToRemove: Shift) {
+    let index = this._shifts.indexOf(shiftToRemove);
+    return this._shifts.splice(index, 1);
   }
 
   /**
    * Get this schedule's shifts that must be filled
    * @returns This schedule's (unassigned) shifts
    */
-  getShifts() {
-    return this.shifts;
+  public get shifts() {
+    return this._shifts;
   }
 
   /**
@@ -38,23 +64,41 @@ export class Schedule {
    * @param newEmp Employee to add to the schedule
    */
   addEmployee(newEmp: Employee) {
-    this.staff.concat(newEmp);
+    this._staff.concat(newEmp);
+  }
+
+  /**
+   * Remove a given employee from the schedule
+   * @param empToRemove Employee to remove from the schedule
+   * @returns the employee removed from the schedule
+   */
+  removeEmployee(empToRemove: Employee) {
+    let index = this._staff.indexOf(empToRemove);
+    return this._staff.splice(index, 1);
   }
 
   /**
    * Get all the employees that exist in the schedule
    * @returns Employees in the schedule
    */
-  getEmployees() {
-    return this.staff;
+  public get employees() {
+    return this._staff;
   }
 
   /**
    * Set the schedule's assigned shifts
    * @param shifts set of assigned shifts (obtained from IScheduleAlgorithm)
    */
-  setAssignedShifts(shifts: Shift[]) {
-    this.assignedShifts.slice(0);
-    this.assignedShifts = shifts;
+  public set assignedShifts(shifts: Shift[]) {
+    this._assignedShifts.slice(0);
+    this._assignedShifts = shifts;
+  }
+
+  /**
+   * Get this schedule's assigned shifts
+   * @returns This schedule's assigned shifts
+   */
+  public get assignedShifts() {
+    return this._assignedShifts;
   }
 }
