e140a2be9a6a7a9df844dfe3989f21db91786825
kantab <brendan.k29@gmail.com>
2022-11-09 15:15:00 -0600

    working on tests

--- __test__/components/addShiftModal.tests.tsx
+++ __test__/components/addShiftModal.tests.tsx
@@ -1,3 +1,9 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { render, fireEvent } from "@testing-library/react";
+import { AddShiftModal } from "../../components/addShiftModal";
+
+test("Add Shift Renders", () => {
+  const addShiftModal = render(<AddShiftModal />);
+  expect(addShiftModal).toMatchSnapshot();
+});
