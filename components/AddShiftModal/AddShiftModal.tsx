8b788af56a37354740a3dba2163384987d73c2c9
kantab <brendan.k29@gmail.com>
2022-11-14 15:20:47 -0600

    Added changes from borked branch

--- components/AddShiftModal/AddShiftModal.tsx
+++ components/AddShiftModal/AddShiftModal.tsx
@@ -1,62 +1,98 @@
-/* import { Button, Modal, Typography } from "@mui/material";
-import Box from "@mui/system/Box/Box";
-import { style } from "@mui/system/style";
-import React from "react";
-
-const Modal = props => {
-    return(
-        <div className ="modal">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h4 className="modal-title">Add Shift Modal</h4>
-                </div>
-                <div className="Modal-body">
-                    This is The Add Shift
-                </div>
-            </div>
-        </div>
-    )
-}
-*/
 import * as React from "react";
+import { Button, Modal, Typography, Card, CardContent } from "@mui/material";
+import modalStyle from "../../styles/modalStyle";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4
-};
-
-export default function BasicModal(): JSX.Element {
+export function AddShiftModal(): JSX.Element {
   const [open, setOpen] = React.useState(false);
-  const handleOpen = (): void => setOpen(true);
-  const handleClose = (): void => setOpen(false);
-
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleOpen = () => setOpen(true);
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleClose = () => setOpen(false);
+  const [DayVal, setDay] = React.useState("");
+  const [valueStartTime, setValueStartTime] = React.useState<Dayjs | null>(
+    null
+  ); // start time variables
+  const [valueEndTime, setValueEndTime] = React.useState<Dayjs | null>(null); // end time variables
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleChange = (event: SelectChangeEvent) => {
+    setDay(event.target.value);
+  };
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+    <>
+      <Button onClick={handleOpen}>Add Shift</Button>
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="addShiftTitle"
+        aria-describedby="addShiftDescription"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            This is the Add Shift Modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}></Typography>
-        </Box>
+        <Card sx={modalStyle}>
+          <CardContent>
+            <Typography
+              id="addShiftTitle"
+              variant="h6"
+              component="h2"
+            ></Typography>
+            <Typography id="selectDay" sx={{ mt: 2 }}></Typography>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="selectDayLabel">Select Day</InputLabel>
+                <Select
+                  labelId="selectDayLabel"
+                  id="demo-simple-select"
+                  value={DayVal}
+                  label="Select Day"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>Sunday</MenuItem>
+                  <MenuItem value={20}>Monday</MenuItem>
+                  <MenuItem value={30}>Tuesday</MenuItem>
+                  <MenuItem value={40}>Wednesday</MenuItem>
+                  <MenuItem value={50}>Thursday</MenuItem>
+                  <MenuItem value={60}>Friday</MenuItem>
+                  <MenuItem value={70}>Saturday</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Typography id="modal-StartTime" sx={{ mt: 2 }}>
+              {" "}
+            </Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="Select Start Time"
+                value={valueStartTime}
+                onChange={(newValueST) => {
+                  setValueStartTime(newValueST);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <Typography id="modal-EndTime" sx={{ mt: 2 }}></Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="Select End Time"
+                value={valueEndTime}
+                onChange={(newValueET) => {
+                  setValueEndTime(newValueET);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <Typography id="modal-submit" sx={{ mt: 2 }}></Typography>
+            <Button variant="contained">Submit</Button>
+          </CardContent>
+        </Card>
       </Modal>
-    </div>
+    </>
   );
 }
