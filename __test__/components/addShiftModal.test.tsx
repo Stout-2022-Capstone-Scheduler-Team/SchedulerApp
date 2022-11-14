9a203d76a74ccb063cb4aa27d6e8f32cb30bfb32
kantab <brendan.k29@gmail.com>
2022-11-14 12:36:55 -0600

    AddShift modal tests modal opening

---
+++ __test__/components/addShiftModal.test.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "@testing-library/react";
+import { AddShiftModal } from "../../components/addShiftModal";
+
+test("Add Shift Renders", () => {
+  const addShiftModal = render(<AddShiftModal />);
+  expect(addShiftModal).toMatchSnapshot();
+});
+test("Modal opens and closes", () => {
+  const modal = render(<AddShiftModal />);
+
+  // Make sure no modal exists
+  {
+    const header = modal.queryByText(/ADD SHIFT/i);
+    expect(modal).toMatchSnapshot();
+  }
+
+  // Click the modal button
+  const openButton = modal.getByText(/ADD SHIFT/i);
+  fireEvent.click(openButton);
+
+  // Make sure the modal exists
+  {
+    const header = modal.queryByText(/ADD SHIFT/i);
+    expect(header).not.toBe(null);
+
+    expect(modal).toMatchSnapshot();
+  }
+
+  // Click the submit button
+  //fireEvent.click(modal.getByText(/Submit /i));
+
+  // Make sure the modal is gone
+  {
+    const header = modal.queryByText(/ADD SHIFT/i);
+    // expect(header).toBe(null);
+
+    expect(modal).toMatchSnapshot();
+  }
+});
--- __test__/components/addShiftModal.tests.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import { render, fireEvent } from "@testing-library/react";
-import { AddShiftModal } from "../../components/addShiftModal";
-
-test("Add Shift Renders", () => {
-  const addShiftModal = render(<AddShiftModal />);
-  expect(addShiftModal).toMatchSnapshot();
-});
