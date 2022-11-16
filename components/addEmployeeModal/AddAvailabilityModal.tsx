1e09861eba2b97cb6faae218eb89fe8dd5b2a446
AndrewUjda <drewjthom@gmail.com>
2022-11-16 14:35:39 -0600

    Just pulling

--- components/addEmployeeModal/AddAvailabilityModal.tsx
+++ components/addEmployeeModal/AddAvailabilityModal.tsx
@@ -24,32 +24,33 @@ import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import modalStyle from "../../styles/modalStyle";
 import { DayOftheWeek, Employee, Shift, Time } from "../../entities";
+import { useNextMonthDisabled } from "@mui/x-date-pickers/internals/hooks/date-helpers-hooks";
 
 interface AddAvailabilityModalProps {
   addAvailability: (shift: Shift) => void;
   day: DayOftheWeek;
   employee?: Employee;
 }
-export function AddAvailabilityModal(props: AddAvailabilityModalProps): JSXElement {
-
+export function AddAvailabilityModal(
+  props: AddAvailabilityModalProps
+): JSXElement {
   const [open, setOpen] = useState(false);
   const [valueStartTime, setValueStartTime] = useState<Dayjs | null>(null);
   const [valueEndTime, setValueEndTime] = useState<Dayjs | null>(null);
 
   const handleOpen = (): void => setOpen(true);
-  const handleClose = (): void =>{ 
-    setOpen(false) 
+  const handleClose = (): void => {
+    setOpen(false);
   };
 
   const handleSubmit = (): void => {
-    setOpen(false)
-    props.addAvailability(new Shift("", new Time(3), new Time(4), props.day, props.employee?.name));
+    setOpen(false);
+    props.addAvailability(
+      new Shift("", new Time(3), new Time(4), props.day, props.employee?.name)
+    );
   };
   const [DayVal, setDay] = useState("");
 
-
-
-  
   const handleChange = (event: SelectChangeEvent) => {
     setDay(event.target.value as string);
   };
@@ -65,16 +66,10 @@ export function AddAvailabilityModal(props: AddAvailabilityModalProps): JSXEleme
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Add Availability
             </Typography>
-            <FormGroup aria-label="position" row>
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Available all day"
-              />
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Shift runs into the next day"
-              />
-            </FormGroup>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Available all day"
+            />
             <FormControl sx={{ mr: 2 }}>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <TimePicker
