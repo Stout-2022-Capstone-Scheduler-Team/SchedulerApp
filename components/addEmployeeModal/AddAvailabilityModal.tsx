b4a7d5bd3fc889a8ebb9f01898ca480ff8a12dd6
AndrewUjda <drewjthom@gmail.com>
2022-11-21 14:33:43 -0600

    Add Availability Tested

--- components/addEmployeeModal/AddAvailabilityModal.tsx
+++ components/addEmployeeModal/AddAvailabilityModal.tsx
@@ -15,14 +15,16 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Dayjs } from "dayjs";
 import { useState } from "react";
 import modalStyle from "../../styles/modalStyle";
-import { DayOftheWeek, Shift, Time } from "../../entities";
+import { dayName, DayOftheWeek, Shift, Time } from "../../entities";
 import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
 
 interface AddAvailabilityModalProps {
   day: DayOftheWeek;
   addAvailability: (shift: Shift) => void;
 }
-export function AddAvailabilityModal(props: AddAvailabilityModalProps): JSX.Element {
+export function AddAvailabilityModal(
+  props: AddAvailabilityModalProps
+): JSX.Element {
   // Props
   const { day, addAvailability } = props;
 
@@ -41,10 +43,14 @@ export function AddAvailabilityModal(props: AddAvailabilityModalProps): JSX.Elem
       clearInputs();
     } else if (validateInputs()) {
       // Is there anything I have to name the shift as?
-      addAvailability(new Shift("", Time.fromDayJs(valueStartTime, day), Time.fromDayJs(valueEndTime, day)));
+      addAvailability(
+        new Shift(
+          "",
+          Time.fromDayJs(valueStartTime, day),
+          Time.fromDayJs(valueEndTime, day)
+        )
+      );
       clearInputs();
-    } else {
-      alert("Please enter a valid time!");
     }
   };
 
@@ -71,22 +77,19 @@ export function AddAvailabilityModal(props: AddAvailabilityModalProps): JSX.Elem
 
   return (
     <>
-      <Button onClick={handleOpen} variant="contained" >
+      <Button onClick={handleOpen} variant="contained">
         Add Availability
       </Button>
       <Modal open={open}>
         <Card sx={modalStyle}>
-          <CardContent sx={{ p: 0, "&:last-child": { p: 0.25 }}}>
+          <CardContent sx={{ p: 0, "&:last-child": { p: 0.25 } }}>
             <Typography id="modal-modal-title" variant="h6" component="h2">
-              Add Availability
+              {dayName(day)} Availability
             </Typography>
             <FormGroup aria-label="position" row>
               <FormControlLabel
-                control={<Checkbox
-                  checked={checked}
-                  onChange={handleChange}
-                />}
-                label="All day"
+                control={<Checkbox checked={checked} onChange={handleChange} />}
+                label="All Day"
               />
             </FormGroup>
             <FormControl sx={{ mr: 2 }}>
