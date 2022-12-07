af4d01b0af506caeabbff58e562784924742ce9b
kantab <brendan.k29@gmail.com>
2022-12-07 13:14:56 -0600

    Style the job Title

--- components/calendar/ShiftCard.tsx
+++ components/calendar/ShiftCard.tsx
@@ -13,8 +13,13 @@ export default function ShiftCard({
   return (
     <Card sx={{ mt: 1, borderLeft: 6, borderColor: employee.color.colorHex }}>
       <CardContent sx={{ p: 0.25, "&:last-child": { p: 0.25 } }}>
+        <Typography
+          sx={{ display: "flex", justifyContent: "center", fontWeight: "bold" }}
+        >
+          {shift.name}
+        </Typography>
         <Typography sx={{ display: "flex", justifyContent: "center" }}>
-          {shift.name} {shift.start.toString()} - {shift.end.toString()}
+          {shift.start.toString()} - {shift.end.toString()}
         </Typography>
         <Typography
           sx={{ fontWeight: "bold", display: "flex", justifyContent: "center" }}
