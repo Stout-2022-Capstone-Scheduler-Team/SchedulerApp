755f072a0025d868378270b06365e45588df4fef
YoungJoel <joeleyoung01@gmail.com>
2022-12-07 13:35:14 -0600

    fix from merge

--- components/addShiftModal/AddShiftModal.tsx
+++ components/addShiftModal/AddShiftModal.tsx
@@ -22,7 +22,6 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { ScheduleAction, Dispatch } from "../../services/scheduleState";
 import { DayOftheWeek, Shift, Time } from "../../entities";
-import react, { useState } from "react";
 
 interface ShiftModalProps {
   existingShifts: Shift[];
