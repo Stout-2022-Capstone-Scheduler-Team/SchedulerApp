038f2ef3c2d90b385b1c9fee6c9c6c0934e675c6
daccola <78117344+daccola@users.noreply.github.com>
2022-12-08 14:11:40 -0600

    added confirmation modal tests

--- __test__/components/EmployeeSummary/EmployeeSummary.test.tsx
+++ __test__/components/EmployeeSummary/EmployeeSummary.test.tsx
@@ -84,6 +84,102 @@ test("Employee Card opens sets addEmployeeModalOpen", async () => {
   expect(setAddEmployeeModalOpen).toBeCalled();
 });
 
-//add coverage for confirmation modal close and delete paths
-// modal opens and closes with buttons
-// modal opens and closes with employee card click
+test("Confirmation modal opens and closes", async () => {
+  const user = userEvent.setup();
+  const dispatch = jest.fn();
+  const employee1 = new Employee("Alice", 0, 40, new Color("Pink"), [
+    new Shift(
+      "",
+      new Time(10, DayOftheWeek.Monday),
+      new Time(12, DayOftheWeek.Monday)
+    )
+  ]);
+  const setCurrentEmployee = jest.fn();
+  const setAddEmployeeModalOpen = jest.fn();
+  const employeeSummary = render(
+    <EmployeeSummary
+      employees={[employee1]}
+      dispatch={dispatch}
+      currentEmployee={null}
+      setCurrentEmployee={setCurrentEmployee}
+      addEmployeeModalOpen={false}
+      setAddEmployeeModalOpen={setAddEmployeeModalOpen}
+    />
+  );
+
+  // Make sure no modal exists
+  {
+    const header = employeeSummary.queryByText(/Confirm Delete/);
+    expect(header).toBe(null);
+  }
+
+  await user.click(
+    employeeSummary.getByTestId(/DeleteIcon/i).parentElement as Element
+  );
+
+  // Make sure the modal exists
+  {
+    const header = employeeSummary.queryByText(/Confirm Delete/);
+    expect(header).not.toBe(null);
+  }
+
+  // Click the cancel button
+  await user.click(employeeSummary.getByText(/Close/));
+
+  // Make sure the modal is gone
+  {
+    const header = employeeSummary.queryByText(/Confirm Delete/);
+    expect(header).toBe(null);
+  }
+});
+
+test("Confirmation modal deletes employee", async () => {
+  const user = userEvent.setup();
+  const dispatch = jest.fn();
+  const employee1 = new Employee("Alice", 0, 40, new Color("Pink"), [
+    new Shift(
+      "",
+      new Time(10, DayOftheWeek.Monday),
+      new Time(12, DayOftheWeek.Monday)
+    )
+  ]);
+  const setCurrentEmployee = jest.fn();
+  const setAddEmployeeModalOpen = jest.fn();
+  const employeeSummary = render(
+    <EmployeeSummary
+      employees={[employee1]}
+      dispatch={dispatch}
+      currentEmployee={null}
+      setCurrentEmployee={setCurrentEmployee}
+      addEmployeeModalOpen={false}
+      setAddEmployeeModalOpen={setAddEmployeeModalOpen}
+    />
+  );
+
+  // Make sure no modal exists
+  {
+    const header = employeeSummary.queryByText(/Confirm Delete/);
+    expect(header).toBe(null);
+  }
+
+  await user.click(
+    employeeSummary.getByTestId(/DeleteIcon/i).parentElement as Element
+  );
+
+  // Make sure the modal exists
+  {
+    const header = employeeSummary.queryByText(/Confirm Delete/);
+    expect(header).not.toBe(null);
+  }
+
+  // Click the delete button
+  await user.click(employeeSummary.getByText(/^Delete$/i));
+
+  expect(dispatch.mock.calls).toEqual([
+    [
+      {
+        remove: employee1
+      }
+    ]
+  ]);
+});
