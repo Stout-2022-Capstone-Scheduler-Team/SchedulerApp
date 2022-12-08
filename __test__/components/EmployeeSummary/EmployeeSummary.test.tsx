60acc87faa94a06fcda3d71a25e92830901c4f1f
daccola <78117344+daccola@users.noreply.github.com>
2022-12-07 23:42:28 -0600

    employee summary tests

--- __test__/components/EmployeeSummary/EmployeeSummary.test.tsx
+++ __test__/components/EmployeeSummary/EmployeeSummary.test.tsx
@@ -1,6 +1,7 @@
 import { EmployeeSummary } from "../../../components";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { Color, DayOftheWeek, Employee, Shift, Time } from "../../../entities";
 
 test("Employee Summary Renders", () => {
   const dispatch = jest.fn();
@@ -9,6 +10,37 @@ test("Employee Summary Renders", () => {
   const employeeSummary = render(
     <EmployeeSummary
       employees={[]}
+      dispatch={dispatch}
+      currentEmployee={null}
+      setCurrentEmployee={setCurrentEmployee}
+      addEmployeeModalOpen={false}
+      setAddEmployeeModalOpen={setAddEmployeeModalOpen}
+    />
+  );
+  expect(employeeSummary).toMatchSnapshot();
+});
+
+test("Employee Summary Renders with employees", () => {
+  const dispatch = jest.fn();
+  const employee1 = new Employee("Alice", 0, 40, new Color("Pink"), [
+    new Shift(
+      "",
+      new Time(10, DayOftheWeek.Monday),
+      new Time(12, DayOftheWeek.Monday)
+    )
+  ]);
+  const employee2 = new Employee("Bob", 0, 40, new Color("Red"), [
+    new Shift(
+      "",
+      new Time(10, DayOftheWeek.Monday),
+      new Time(12, DayOftheWeek.Monday)
+    )
+  ]);
+  const setCurrentEmployee = jest.fn();
+  const setAddEmployeeModalOpen = jest.fn();
+  const employeeSummary = render(
+    <EmployeeSummary
+      employees={[employee1, employee2]}
       dispatch={dispatch}
       currentEmployee={null}
       setCurrentEmployee={setCurrentEmployee}
